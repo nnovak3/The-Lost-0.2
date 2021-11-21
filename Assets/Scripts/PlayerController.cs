a657b79f367a37c44366a94cd201e61f5714e185
Austin <aellio23@emich.edu>
2021-11-21 14:13:08 -0500

    Added player and weapon
    Added player and weapon and testing to see if merging works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public GameObject healthBar;
+    public float speed;
+    private Slider healthSlider;
+    private float playerHealth;
+    
     private Vector3 move;
     private float deltaX;
     private float deltaY;
@@ -13,6 +20,8 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
+        playerHealth = 100f;
+        //healthSlider = healthBar.GetComponentInChildren<Slider>();
     }
 
     private void Update()
@@ -25,24 +34,31 @@ public class PlayerController : MonoBehaviour
         // Swap sprite direction; if player is moving right, default sprite orientation. If player moving left, flip the sprite by setting scale.x to -1
         if(move.x > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(2, 2, 2);
         }else if(move.x < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-2, 2, 2);
         }
 
         // We check to see if the player will collide with anything before moving them
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, move.y), Mathf.Abs(move.y * Time.deltaTime), LayerMask.GetMask("Player", "Collidable"));
         if (hit.collider == null) // BoxCast() returns a RayCastHit2D object; the Collider property of the result will be NULL if nothing was hit, meaning the player can move
         {
-            transform.Translate(0, move.y * Time.deltaTime, 0);
+            transform.Translate(0, move.y * Time.deltaTime * speed, 0);
         }
         // We just checked for collisions in the y direction, now we need to check x
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(move.x, 0), Mathf.Abs(move.x * Time.deltaTime), LayerMask.GetMask("Player", "Collidable"));
         if (hit.collider == null) 
         {
-            transform.Translate(move.x * Time.deltaTime, 0, 0);
+            transform.Translate(move.x * Time.deltaTime * speed, 0, 0);
         }
 
+        //healthSlider.value = playerHealth;
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
     }
 }

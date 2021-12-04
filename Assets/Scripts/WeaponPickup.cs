c163b398b26221ba3a0820c73d0750021c1f2b2b
danegrimm <dgrimm2@emich.edu>
2021-12-04 15:40:57 -0500

    added pickup script, and weapon control
    added a pick up script to the pistol but it destroys the object after pick up instead of picking it up and replacing the knife.  also added the weapon control script to the pistol

--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -4,34 +4,20 @@ using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
-    public GameObject player;
-    public PlayerCombat combatScript;
-    public Rigidbody rb;
-    public BoxCollider coll;
-    public Transform player1;
-    public float pickUpRange;
-    public bool equipped;
-    public static bool slotFull;
-
-    private void Start()
+    void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");        
-    }
-
-    public void Update()
-    {
-        Vector2 distanceToPlayer = player1.position - transform.position;
-        if(!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.F) && !slotFull) PickUp();
-
         
     }
-    
-    private void PickUp()
-    {
-        equipped = true;
-        slotFull = true;
-        combatScript.enabled = true;
-    }
 
-    
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
